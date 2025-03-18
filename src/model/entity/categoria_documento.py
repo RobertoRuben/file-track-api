28c9054211a0047e520a33117e1778901dca979a
RobertoRuben <robertoch263@gmail.com>
2025-03-17 21:09:57 -0500

    🗃️ feat(entity): añadir modelo CategoriaDocumento

---
+++ src/model/entity/categoria_documento.py
@@ -0,0 +1,29 @@
+from sqlmodel import (
+    SQLModel,
+    text,
+    Field,
+    CheckConstraint,
+    Column,
+    BIGINT,
+    TEXT,
+    DateTime
+)
+from datetime import datetime
+
+class CategoriaDocumento(SQLModel):
+    __tablename__ = "categorias_documento"
+    __table_args__ = (
+        CheckConstraint("LENGTH(name) > 3", name="ck_categoria_documento_name"),
+    )
+
+    id: int | None = Field(default=None, sa_column=Column(BIGINT, primary_key=True))
+    name: str = Field(sa_column=Column(TEXT, nullable=False, unique=True))
+    created_at: datetime | None = Field(
+        sa_column=Column(
+            DateTime(timezone=True), server_default=text("CURRENT_TIMESTAMP AT TIME ZONE 'UTC'")
+        ),
+    )
+    updated_at: datetime | None = Field(
+        default=None, sa_column=Column(DateTime(timezone=True))
+    )
+
