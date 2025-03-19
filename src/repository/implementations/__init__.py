69b9f5f322a5a99394c614433b936e005dad1ab3
RobertoRuben <robertoch263@gmail.com>
2025-03-19 10:19:31 -0500

    ✨ feat(repository): implementar RolRepositoryImpl para gestión de roles

--- src/repository/implementations/__init__.py
+++ src/repository/implementations/__init__.py
@@ -1,3 +1,7 @@
 from .categoria_repository_impl import CategoriaRepositoryImpl
+from .rol_repository_impl import RolRepositoryImpl
 
-__all__ = ["CategoriaRepositoryImpl"]
+__all__ = [
+    "CategoriaRepositoryImpl",
+    "RolRepositoryImpl",
+]
