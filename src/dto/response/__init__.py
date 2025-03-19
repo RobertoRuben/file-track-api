e49f3a862015dbb35ae73b556880b3b9533eb674
RobertoRuben <robertoch263@gmail.com>
2025-03-19 11:57:32 -0500

    🔧 chore(dto): agregar DTOs para solicitud y respuesta de roles

--- src/dto/response/__init__.py
+++ src/dto/response/__init__.py
@@ -1,6 +1,9 @@
 from .category_document_response_dto import CategoryDocumentResponseDTO, CategoryDocumentPage
+from .role_response_dto import RoleResponseDTO, RolePage
 
 __all__ = [
     "CategoryDocumentResponseDTO",
-    "CategoryDocumentPage"
+    "CategoryDocumentPage",
+    "RoleResponseDTO",
+    "RolePage",
 ]
