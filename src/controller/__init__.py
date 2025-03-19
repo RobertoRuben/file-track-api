7bc2402112ca1f9bdabf7e625071142962481cf4
RobertoRuben <robertoch263@gmail.com>
2025-03-18 20:30:55 -0500

    🔧 chore(controller): export category_document_tags_metadata for improved module access

--- src/controller/__init__.py
+++ src/controller/__init__.py
@@ -1,3 +1,3 @@
-from .category_document_controller import router as category_document_router
+from .category_document_controller import router as category_document_router, category_document_tags_metadata
 
-__all__ = ["category_document_router"]
+__all__ = ["category_document_router", "category_document_tags_metadata"]
