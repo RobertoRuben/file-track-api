08a39116c5b14c7c9643f36836719a7d6e33dce4
RobertoRuben <robertoch263@gmail.com>
2025-03-19 11:59:49 -0500

    ✨ feat(dependency): agregar función para obtener el servicio de roles

--- src/service/dependencies/__init__.py
+++ src/service/dependencies/__init__.py
@@ -1,3 +1,7 @@
 from .category_document_dependency import get_category_document_service
+from .role_service_dependency import get_role_service
 
-__all__ = ["get_category_document_service"]
+__all__ = [
+    "get_category_document_service",
+    "get_role_service"
+]
