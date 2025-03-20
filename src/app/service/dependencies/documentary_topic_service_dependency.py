b2c925c8201508f0216146311962bb2619bcc636
RobertoRuben <robertoch263@gmail.com>
2025-03-20 14:23:36 -0500

    ✨ feat(dependency): agregar dependencia para el servicio de ambito documental

---
+++ src/app/service/dependencies/documentary_topic_service_dependency.py
@@ -0,0 +1,21 @@
+from fastapi import Depends
+from src.app.service.interfaces import IDocumentaryTopicService
+from src.app.service.implementations import DocumentaryTopicServiceImpl
+from src.app.repository.interfaces import IDocumentaryTopicRepository
+from src.app.repository.dependencies import get_documentary_topic_repository
+
+async def get_department_service(repository: IDocumentaryTopicRepository = Depends(get_documentary_topic_repository)) -> IDocumentaryTopicService:
+    """
+    Dependency function to get the department service implementation.
+
+    This function creates and provides an instance of the department service
+    implementation with the necessary repository dependency injected.
+
+    Args:
+        repository: The department repository implementation provided by
+                    the FastAPI dependency injection system.
+
+    Returns:
+        An implementation of IDepartmentService configured with the provided repository.
+    """
+    return DocumentaryTopicServiceImpl(repository=repository)
