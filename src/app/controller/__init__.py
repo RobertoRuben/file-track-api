04e076466459035b27dda22a70c6dd38eaf2f46a
RobertoRuben <robertoch263@gmail.com>
2025-03-20 09:59:10 -0500

    ✨ feat(controller): implementar el controllador para la gestion de áreas

--- src/app/controller/__init__.py
+++ src/app/controller/__init__.py
@@ -1,9 +1,12 @@
 from .category_document_controller import router as category_document_router, category_document_tags_metadata
 from .role_controller import router as role_router, role_tags_metadata
+from .department_controller import router as department_router, department_tags_metadata
 
 __all__ = [
     "category_document_router",
     "category_document_tags_metadata",
     "role_router",
-    "role_tags_metadata"
+    "role_tags_metadata",
+    "department_router",
+    "department_tags_metadata"
 ]
