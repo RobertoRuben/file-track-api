c6327ddf16c93ec22ab5aa3723d01c338835fec9
RobertoRuben <robertoch263@gmail.com>
2025-03-24 10:57:12 -0500

    🗃️ feat(entity): agregar modelo Remitente con validaciones y restricciones

--- src/app/model/entity/__init__.py
+++ src/app/model/entity/__init__.py
@@ -3,6 +3,7 @@ from .rol import Rol
 from .area import Area
 from .ambito import Ambito
 from .centro_poblado import CentroPoblado
+from .remitente import Remitente
 
 __all__ = [
     "CategoriaDocumento",
@@ -10,4 +11,5 @@ __all__ = [
     "Area",
     "Ambito",
     "CentroPoblado",
+    "Remitente",
 ]
