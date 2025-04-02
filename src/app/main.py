ef19c644add39ec2085e8e173181763ff95ae2ff
RobertoRuben <robertoch263@gmail.com>
2025-04-02 08:31:55 -0500

    ✨ feat(main): agregar enrutador para conexiones de área en la API

--- src/app/main.py
+++ src/app/main.py
@@ -21,6 +21,8 @@ from src.app.controller import (
     employee_tags_metadata,
     hamlet_router,
     hamlet_tags_metadata,
+    area_router,
+    area_connection_tags_metadata,
 )
 
 API_PREFIX = "/api/v1"
@@ -35,6 +37,7 @@ tags_metadata = [
     position_tags_metadata,
     employee_tags_metadata,
     hamlet_tags_metadata,
+    area_connection_tags_metadata,
 ]
 
 
@@ -78,3 +81,4 @@ app.include_router(submitter_router, prefix=API_PREFIX)
 app.include_router(position_router, prefix=API_PREFIX)
 app.include_router(employee_router, prefix=API_PREFIX)
 app.include_router(hamlet_router, prefix=API_PREFIX)
+app.include_router(area_router, prefix=API_PREFIX)
