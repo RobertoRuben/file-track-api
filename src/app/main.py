ee6387e99ceef2d3e8089df4a593a52cba73e90a
RobertoRuben <robertoch263@gmail.com>
2025-04-04 10:08:03 -0500

    ♻️ refactor(main): rename document category router to document category tags metadata for improved clarity

--- src/app/main.py
+++ src/app/main.py
@@ -30,7 +30,7 @@ from src.app.controller import (
 API_PREFIX = "/api/v1"
 
 tags_metadata = [
-    document_category_router,
+    document_category_tags_metadata,
     role_tags_metadata,
     department_tags_metadata,
     documentary_topic_tags_metadata,
