342d14c13e9a79b6197be5125e6f301f79eb3e98
RobertoRuben <robertoch263@gmail.com>
2025-04-08 12:29:26 -0500

    ✨ feat(dependency): add requires_scopes to manage scope requirements for protected resources

--- src/app/service/dependencies/__init__.py
+++ src/app/service/dependencies/__init__.py
@@ -11,6 +11,7 @@ from .department_connection_service_dependency import get_department_connection_
 from .user_service_dependency import get_user_service
 from .auth_service_dependency import get_auth_service
 from .auth_current_user_dependency import get_current_user
+from .auth_scope_dependency import requires_scopes
 
 __all__ = [
     "get_document_category_service",
@@ -26,4 +27,5 @@ __all__ = [
     "get_user_service",
     "get_auth_service",
     "get_current_user",
+    "requires_scopes",
 ]
