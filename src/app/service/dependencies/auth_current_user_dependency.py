8c137c00085185199767467ff9032e3583017d57
RobertoRuben <robertoch263@gmail.com>
2025-04-08 12:29:14 -0500

    ✨ feat(dependency): update current user retrieval to include security scope validation

--- src/app/service/dependencies/auth_current_user_dependency.py
+++ src/app/service/dependencies/auth_current_user_dependency.py
@@ -1,28 +1,30 @@
 from fastapi import Depends
-from fastapi.security import OAuth2PasswordBearer
-from typing import Annotated
-
-from src.app.dto.response import UserResponseDTO
+from fastapi.security import OAuth2PasswordBearer, SecurityScopes
+from src.app.dto.response import CurrentUserResponseDTO
 from src.app.service.interfaces import IAuthService
 from src.app.service.dependencies import get_auth_service
+from src.app.security.auth.constants import scope_descriptions
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/api/v1/auth/login", scopes=scope_descriptions
+)
 
 
 async def get_current_user(
-    token: Annotated[str, Depends(oauth2_scheme)],
+    security_scopes: SecurityScopes,
+    token: str = Depends(oauth2_scheme),
     auth_service: IAuthService = Depends(get_auth_service),
-) -> UserResponseDTO:
+) -> CurrentUserResponseDTO:
     """
-    Dependency function to retrieve the current user from a Bearer token.
+    Dependency that verifies the current user based on the provided token
+    and checks if the required security scopes are satisfied.
 
-    This function extracts the token using `oauth2_scheme` and calls
-    `auth_service.get_current_user(token)` to validate and decode the token.
-    If the token is valid, it returns a `UserResponseDTO` with the authenticated user's data.
-    Otherwise, it raises a 401 exception for invalid or expired tokens.
-
-    :param token: The Bearer token extracted by the FastAPI dependency injection system
-    :param auth_service: The authentication service used to validate the token and retrieve the current user
-    :return: A `UserResponseDTO` containing the information of the authenticated user
+    :param security_scopes: Scopes required by the endpoint
+    :param token: JWT access token extracted via OAuth2PasswordBearer
+    :param auth_service: The authentication service implementation
+    :return: CurrentUserResponseDTO containing the current user's information
     """
-    return await auth_service.get_current_user(token)
+    return await auth_service.get_current_user_with_scopes(
+        token,
+        security_scopes.scopes,
+    )
