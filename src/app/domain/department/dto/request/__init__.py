7e79723990a15906a13e0a93672d95bdac962df1
RobertoRuben <robertoch263@gmail.com>
2025-07-09 12:30:25 -0500

    ♻️ refactor: condense and format __all__ declarations in __init__.py files for consistency

--- src/app/domain/department/dto/request/__init__.py
+++ src/app/domain/department/dto/request/__init__.py
@@ -1,7 +1,4 @@
 from .department_request_dto import DepartmentRequestDTO
 from .department_connection_request_dto import DepartmentConnectionRequestDTO
 
-__all__ = [
-    "DepartmentRequestDTO",
-    "DepartmentConnectionRequestDTO"
-]
+__all__ = ["DepartmentRequestDTO", "DepartmentConnectionRequestDTO"]
