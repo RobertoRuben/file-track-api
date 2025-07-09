ea9793e686b4f9787ace8fde1dd7817edc7cb904
RobertoRuben <robertoch263@gmail.com>
2025-07-09 13:15:59 -0500

    ♻️ refactor(submitter): move submitter model to root, update Document import path for consistency

--- src/app/domain/submitter/model/submitter.py
+++ src/app/domain/submitter/model/submitter.py
@@ -13,7 +13,7 @@ from datetime import datetime
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from .document import Document
+    from src.app.domain.document.model import Document
 
 
 class Submitter(SQLModel, table=True):
