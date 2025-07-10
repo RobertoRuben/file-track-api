bb4449862435991fb7b85dd07844164869201b2e
RobertoRuben <robertoch263@gmail.com>
2025-07-09 21:12:31 -0500

    ♻️ refactor(document): rename document_controller export to document_router for consistency

--- src/app/domain/document/controller/__init__.py
+++ src/app/domain/document/controller/__init__.py
@@ -2,7 +2,10 @@ from .document_category_controller import (
     router as document_category_router,
     document_category_tags_metadata,
 )
-from .document_controller import router as document_controller, document_tags_metadata
+from .document_controller import (
+    router as document_router,
+    document_tags_metadata
+)
 from .documentary_topic_controller import (
     router as documentary_topic_router,
     documentary_topic_tags_metadata,
@@ -11,7 +14,7 @@ from .documentary_topic_controller import (
 __all__ = [
     "document_category_router",
     "document_category_tags_metadata",
-    "document_controller",
+    "document_router",
     "document_tags_metadata",
     "documentary_topic_router",
     "documentary_topic_tags_metadata",
