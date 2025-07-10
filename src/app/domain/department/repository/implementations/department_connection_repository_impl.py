c228d1c158afeb159a4b65eca33343e14cfd65ae
RobertoRuben <robertoch263@gmail.com>
2025-07-09 22:05:29 -0500

    ♻️ refactor: update import paths for department and user domain modules to match new structure

--- src/app/domain/department/repository/implementations/department_connection_repository_impl.py
+++ src/app/domain/department/repository/implementations/department_connection_repository_impl.py
@@ -6,7 +6,7 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from src.app.core.schema import Page, Pagination
 from src.app.core.db.decorator import transactional
 from src.app.core.exception import InvalidFieldException
-from src.app.domain.document.repository.interface import (
+from src.app.domain.department.repository.interface import (
     IDepartmentConnectionRepository,
 )
 from src.app.domain.department.model import DepartmentConnection, Department
--- src/app/domain/user/service/interface/user_service.py
+++ src/app/domain/user/service/interface/user_service.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+
 from src.app.core.schema import MessageResponse
 from src.app.domain.user.dto.request import UserRequestDTO
 from src.app.domain.user.dto.response import UserResponseDTO, UserPage
