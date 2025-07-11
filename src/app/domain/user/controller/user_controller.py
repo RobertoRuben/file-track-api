1977631c8759286721b743198fe6bdd2b5e3c90c
RobertoRuben <robertoch263@gmail.com>
2025-07-11 16:56:37 -0500

    ✨ feat(user): add unified exception handling decorator to all controller endpoints

--- src/app/domain/user/controller/user_controller.py
+++ src/app/domain/user/controller/user_controller.py
@@ -1,13 +1,5 @@
 from datetime import datetime
-from fastapi import (
-    APIRouter,
-    Depends,
-    Query,
-    Security,
-    Body,
-    Request,
-    Response
-)
+from fastapi import APIRouter, Depends, Query, Security, Body, Request, Response
 from src.app.core.exception.schema import (
     BackRequestError,
     ConflictError,
